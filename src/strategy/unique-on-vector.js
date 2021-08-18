c99108a5f3d459074d218c7278bb3cf42f577277
altintx <bbosh@alternateinterior.com>
2021-08-17 19:07:58 -0600

    strategy was doing unneccessary work

--- src/strategy/unique-on-vector.js
+++ src/strategy/unique-on-vector.js
@@ -1,26 +1,6 @@
 import { dropCandidate, unique, cellFactory, row, col, block } from '../App';
 
-export function __uniqueOnAVector(newGrid, cell, index, vector, prop, addLog) {
-  let cont = true;
-  // is a particular number a candidate in a single cell
-  cell.candidates.forEach(candidate => {
-    if(cont && !(vector(newGrid, cell[prop])
-      .filter(other => !cell.is(other))
-      .reduce((instances, other) => instances.concat(other.candidates), [])
-      .filter(unique)
-      .includes(candidate))
-    ) {
-      if(!cell.value) {
-        newGrid[index] = cellFactory(candidate, [], cell.block, cell.row, cell.col);
-      }
-      cont = false;
-    }
-  });
-  return cont;
-}
-
 function eliminateCandidates(other, grid, cell, addLog) {
-    if(cell === other) return;
     if(!cell.value) return;
     dropCandidate(grid, other, cell.value, addLog);
   }
@@ -30,10 +10,11 @@ export default function uniqueOnAVector({ grid, addLog }) {
   // elim candidates in row 
   newGrid.forEach((cell, index) => {
     [[row, "row"], [col, "col"], [block, "block"]].map(([f, p]) => 
-      f(newGrid, cell[p]).forEach(other => eliminateCandidates(other, newGrid, cell, (cell, action) => addLog(cell, action, `Candidate was assigned elsewhere on ${p}`))))
-    __uniqueOnAVector(newGrid, cell, index, row, 'row', addLog) &&
-    __uniqueOnAVector(newGrid, cell, index, col, 'col', addLog) &&
-    __uniqueOnAVector(newGrid, cell, index, block, 'block', addLog);
+      f(newGrid, cell[p]).forEach(other => {
+        if(cell.is(other)) return;
+        const limiter = cell;
+        eliminateCandidates(other, newGrid, cell, (cell, action) => addLog(cell, action, `Candidate was assigned at (${limiter.row+1}, ${limiter.col+1})`));
+      }))
   });
   return newGrid;
 }
